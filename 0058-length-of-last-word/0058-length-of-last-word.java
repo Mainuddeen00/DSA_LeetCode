4cfc9247faf6eaa7194a85d285eff89fa1f789b8
Mainuddeen <150521034+Mainuddeen00@users.noreply.github.com>
2026-07-30 23:04:56 +0530

    Time: 0 ms (100%), Space: 42.8 MB (84.57%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        int n = s.length();
+
+        int count = 0;
+        int countspace = 0;
+
+        for(int i = n-1; i>=0; i--){
+
+            char chr = s.charAt(i);
+
+            if(chr != ' '){
+
+                count++;
+            }else{
+
+                if(count > 0){
+
+                    break;
+                }
+            }
+
+        }
+
+        return count;
+        
+    }
+}

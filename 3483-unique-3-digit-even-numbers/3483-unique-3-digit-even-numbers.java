d373e6373ae7cda8bd456da01c959a8f9fe8aa15
Mainuddeen <150521034+Mainuddeen00@users.noreply.github.com>
2026-09-18 23:42:27 +0530

    Time: 10 ms (15.68%), Space: 47.2 MB (8.03%) - LeetHub

---
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] availableCounts = new int[10];
+        for (int digit : digits) {
+            availableCounts[digit]++;
+        }
+
+        int distinctCount = 0;
+        for (int num = 100; num <= 998; num += 2) {
+            int hundreds = num / 100;
+            int tens = (num / 10) % 10;
+            int units = num % 10;
+
+           int[] requiredCounts = new int[10];
+            requiredCounts[hundreds]++;
+            requiredCounts[tens]++;
+            requiredCounts[units]++;
+
+            boolean canForm = true;
+            for (int i = 0; i < 10; i++) {
+                if (requiredCounts[i] > availableCounts[i]) {
+                    canForm = false;
+                    break;
+                }
+            }
+
+            if (canForm) {
+                distinctCount++;
+            }
+        }
+
+        return distinctCount;
+    }
+}
